3b3fb8310a46c4413ce813581ba858b8e95a36d0
vadimpriakhin-oss <vadim.priakhin@gmail.com>
2026-03-29 13:10:59 +0300

    Update service-worker.js to change '/script.js' to '/app.js' and add '/data.js' to the urlsToCache.

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,22 @@
-'use strict';
-
-const CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js'
+const FILES_TO_CACHE = [
+  '/index.html',
+  '/app.js',
+  '/data.js',
+  '/styles.css'
 ];
 
-// Install the service worker
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open('your-cache-name').then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
-// Fetch cached resources
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
-});
-
-// Activate the service worker
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
